bc28b18f2dc61f1722c7834a574a45af3bce1ab0
Kevin <29125263+ilikecubes@users.noreply.github.com>
2023-03-28 21:45:23 +0200

    Added Missing Provider Export

--- lib/cube_framework.dart
+++ lib/cube_framework.dart
@@ -6,6 +6,7 @@ export 'src/src.dart';
 // export usefull thridparty libraries
 export 'package:flutter_hooks/flutter_hooks.dart';
 export 'package:provider/provider.dart' hide Dispose;
+export 'package:provider/single_child_widget.dart';
 export 'package:url_launcher/url_launcher.dart';
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:google_fonts/google_fonts.dart';
