e84115c56dc3c75a7b186b9741cff7fca0695c12
Kevin <scheeren@proton.me>
2023-04-13 14:24:17 +0200

    Added Missing Exports

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -1,2 +1,4 @@
 export 'cube_framework_consumer_widget.dart';
 export 'cube_framework_widget.dart';
+export 'cube_framework_responsive_consumer_widget.dart';
+export 'cube_framework_responsive_widget.dart';
